c98382084cb7835524eecec939823222fe01dca9
fhusain01 <fhusain@adaptready.com>
2022-12-17 22:00:24 +0530

    Setup the express json middleware

--- app.js
+++ app.js
@@ -6,3 +6,6 @@ const app = express();
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
