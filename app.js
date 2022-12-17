9318a72f0abf032c74be465760a65bfe6c559b57
fhusain01 <fhusain@adaptready.com>
2022-12-17 21:59:22 +0530

    Imported error-handler and nor-found middleware in app.js

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
