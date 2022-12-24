366ba307f3be33b2659e61e841dc383adf7a353f
fhusain01 <fhusain@adaptready.com>
2022-12-24 20:38:39 +0530

    Used skip and limit to approach the pagination functionality

--- controllers/products.js
+++ controllers/products.js
@@ -38,6 +38,13 @@ const getAllProducts = async (req, res) => {
         result.select(fieldsList)
     }
 
+    // For pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
     const products = await result;
 
     return res.status(200).json({ products, nbHits: products.length });
