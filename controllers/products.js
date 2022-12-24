65df1b335912c3acd90a37b0be8e1c65102c59df
fhusain01 <fhusain@adaptready.com>
2022-12-24 15:48:33 +0530

    Added functionality for the static route

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,8 @@
+const Product = require('../models/Product');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors');
-    return res.status(200).json({ msg: 'Testing static products route' });
+    const products = await Product.find({});
+    return res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
